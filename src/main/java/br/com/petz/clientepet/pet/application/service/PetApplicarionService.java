f1f8fa073215c8dd20965214ba60e0a54983e456
devxavierr <devxavierr@gmail.com>
2023-09-12 17:31:08 -0300

    implementa fluxo service

--- src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicarionService.java
+++ src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicarionService.java
@@ -2,12 +2,18 @@ package br.com.petz.clientepet.pet.application.service;
 
 import br.com.petz.clientepet.pet.application.api.PetRequest;
 import br.com.petz.clientepet.pet.application.api.PetResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
+@Service
+@Log4j2
 public class PetApplicarionService implements PetService {
     @Override
+
     public PetResponse criaPet(UUID idCliente, PetRequest petRequest) {
+        log.info("[inicia] PetApplicarionService - criaPet");
+        log.info("[finaliza] PetApplicarionService - criaPet");
         return null;
     }
 }
